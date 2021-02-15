f8537a6bae4d232e910729e8b78e1adc553cbf9e
Robin <robin@localhost.co>
2021-02-15 14:35:00 +0200

    Install Docker through Ansible

--- aws.tf
+++ aws.tf
@@ -27,6 +27,13 @@ resource "aws_security_group" "allow_ssh" {
     to_port   = 22
     protocol  = "tcp"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "linux" {
